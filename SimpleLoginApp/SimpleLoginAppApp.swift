4b241a1edb265827a5eddd5d87e6922b83da3cbf
matangans23 <matangans@gmail.com>
2023-05-08 21:29:50 -0400

    Initial Commit

---
+++ SimpleLoginApp/SimpleLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginAppApp.swift
+//  SimpleLoginApp
+//
+//  Created by Matan Gans on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
